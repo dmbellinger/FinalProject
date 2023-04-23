a6232c0a527608e96154d7fc5edabaee525021d2
dmbellinger <dmbellinger@quinnipiac.edu>
2023-04-23 17:34:01 -0400

    Added Save fragment and edited view list fragment

--- app/src/main/java/com/hfad/finalproject/ViewListFragment.kt
+++ app/src/main/java/com/hfad/finalproject/ViewListFragment.kt
@@ -1,10 +1,15 @@
 package com.hfad.finalproject
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -17,43 +22,41 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class ViewListFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var itemList: MutableList<Item>
+    private lateinit var itemAdapter: ItemAdapter
 
+    @SuppressLint("MissingInflatedId")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_view_list, container, false)
-    }
+        val view = inflater.inflate(R.layout.fragment_view_list, container, false)
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ViewListFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ViewListFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+
+        itemAdapter = ItemAdapter()
+        val recyclerView: RecyclerView = view.findViewById(R.id.itemRecyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(activity)
+        recyclerView.adapter = itemAdapter
+
+        val deleteButton: Button = view.findViewById(R.id.deleteButton)
+        deleteButton.setOnClickListener {
+            view.findNavController()
+                .navigate(R.id.action_viewListFragment_to_deleteFragment)
+        }
+
+        val addButton: Button = view.findViewById(R.id.AddScreenButton)
+        addButton.setOnClickListener {
+            view.findNavController()
+                .navigate(R.id.action_viewListFragment_to_addFragment)
+        }
+
+        val savedListsButton: Button = view.findViewById(R.id.SaveButton)
+        savedListsButton.setOnClickListener {
+            view.findNavController()
+                .navigate(R.id.action_viewListFragment_to_savedFragment)
+        }
+
+        return view
     }
 }
